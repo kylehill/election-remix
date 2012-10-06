edacc9edabb0b2c8acc04ec611b96ab1f06a0c3e
Kyle Hill <kyle@everfi.com>
2012-10-06 14:22:28 -0400

    pulls 538 data, runs simulation

--- data.js
+++ data.js
@@ -1,65 +1,27 @@
-var data = {
-  "Alaska":{"name":"Alaska","moe":7.2,"dem":39.8,"rep":58.4},
-  "Alabama":{"name":"Alabama","moe":4.2,"dem":36.7,"rep":62.5},
-  "Arkansas":{"name":"Arkansas","moe":4.9,"dem":39.1,"rep":59},
-  "Arizona":{"name":"Arizona","moe":4.9,"dem":45.5,"rep":53.3},
-  "California":{"name":"California","moe":4.4,"dem":59.6,"rep":39.1},
-  "Colorado":{"name":"Colorado","moe":4.8,"dem":51.2,"rep":47.9},
-  "Connecticut":{"name":"Connecticut","moe":4.8,"dem":56.5,"rep":42.7},
-  "District of Columbia":{"name":"District of Columbia","moe":5.3,"dem":93.4,"rep":5.7},
-  "Delaware":{"name":"Delaware","moe":6.4,"dem":59.9,"rep":39.2},
-  "Florida":{"name":"Florida","moe":4.3,"dem":50.6,"rep":48.8},
-  "Georgia":{"name":"Georgia","moe":4.7,"dem":44.5,"rep":54.8},
-  "Hawaii":{"name":"Hawaii","moe":5.7,"dem":67,"rep":31.8},
-  "Iowa":{"name":"Iowa","moe":5.1,"dem":51.1,"rep":47.6},
-  "Idaho":{"name":"Idaho","moe":6.7,"dem":32.9,"rep":65.3},
-  "Illinois":{"name":"Illinois","moe":4.6,"dem":60.3,"rep":38.5},
-  "Indiana":{"name":"Indiana","moe":4.7,"dem":45.8,"rep":53.1},
-  "Kansas":{"name":"Kansas","moe":7,"dem":38.9,"rep":59.8},
-  "Kentucky":{"name":"Kentucky","moe":4.9,"dem":41,"rep":58},
-  "Louisiana":{"name":"Louisiana","moe":4.2,"dem":41,"rep":57.4},
-  "Massachusetts":{"name":"Massachusetts","moe":5.6,"dem":59.7,"rep":39},
-  "Maryland":{"name":"Maryland","moe":4.2,"dem":60.5,"rep":38.2},
-  "Michigan":{"name":"Michigan","moe":4.3,"dem":53.7,"rep":44.9},
-  "Minnesota":{"name":"Minnesota","moe":4.5,"dem":53.4,"rep":44.9},
-  "Missouri":{"name":"Missouri","moe":4.3,"dem":46.7,"rep":52.2},
-  "Mississippi":{"name":"Mississippi","moe":5.7,"dem":39.6,"rep":59.6},
-  "Montana":{"name":"Montana","moe":5.7,"dem":45.2,"rep":52.6},
-  "North Carolina":{"name":"North Carolina","moe":4,"dem":49.1,"rep":50.3},
-  "North Dakota":{"name":"North Dakota","moe":5.5,"dem":42.6,"rep":56},
-  "New Hampshire":{"name":"New Hampshire","moe":5.6,"dem":53,"rep":46.2},
-  "New Jersey":{"name":"New Jersey","moe":4.7,"dem":55.6,"rep":43.3},
-  "New Mexico":{"name":"New Mexico","moe":5.1,"dem":54.1,"rep":44.6},
-  "Nevada":{"name":"Nevada","moe":4.6,"dem":51.9,"rep":47},
-  "New York":{"name":"New York","moe":4.3,"dem":62.6,"rep":36.8},
-  "Ohio":{"name":"Ohio","moe":4.3,"dem":51.7,"rep":47.1},
-  "Oklahoma":{"name":"Oklahoma","moe":5,"dem":34.2,"rep":65.5},
-  "Oregon":{"name":"Oregon","moe":5.4,"dem":55.9,"rep":42.2},
-  "Pennsylvania":{"name":"Pennsylvania","moe":4,"dem":53.5,"rep":45.4},
-  "Rhode Island":{"name":"Rhode Island","moe":6.2,"dem":62.4,"rep":36},
-  "South Carolina":{"name":"South Carolina","moe":5.3,"dem":43.4,"rep":55.6},
-  "South Dakota":{"name":"South Dakota","moe":6.1,"dem":42.3,"rep":56.2},
-  "Tennessee":{"name":"Tennessee","moe":4.9,"dem":41.7,"rep":57.3},
-  "Texas":{"name":"Texas","moe":4.4,"dem":42.6,"rep":57},
-  "Utah":{"name":"Utah","moe":5.9,"dem":29.3,"rep":68.8},
-  "Virginia":{"name":"Virginia","moe":4,"dem":50.9,"rep":48.2},
-  "Vermont":{"name":"Vermont","moe":6,"dem":67.2,"rep":31.3},
-  "Washington":{"name":"Washington","moe":5,"dem":57,"rep":41.4},
-  "Wisconsin":{"name":"Wisconsin","moe":4.7,"dem":52.9,"rep":46.4},
-  "West Virginia":{"name":"West Virginia","moe":5.9,"dem":42.1,"rep":56.5},
-  "Wyoming":{"name":"Wyoming","moe":7,"dem":32.1,"rep":66.2},
-  "Maine":{"name":"Maine Statewide","moe":5.5,"dem":57,"rep":41.4,
-    "districts":[
-      {"name":"Maine CD-1 District 1","moe":5.7,"dem":60.1,"rep":38.5},
-      {"name":"Maine CD-2 District 2","moe":6.2,"dem":53.7,"rep":44.5}
-    ]
-  },
-  "Nebraska":{"name":"Nebraska Statewide","moe":4.6,"dem":39.4,"rep":59.3,
-    "districts":[
-      {"name":"Nebraska CD-1 District 1","moe":5.9,"dem":41.9,"rep":56.8},
-      {"name":"Nebraska CD-2 District 2","moe":4.9,"dem":46.5,"rep":52.1},
-      {"name":"Nebraska CD-3 District 3","moe":6,"dem":28.5,"rep":70.1}
-    ]
-  }
-}
+var massage = function(data) {
+  var nowcast = data.states.nowcast[0].states;
   
+  var data = {};
+  
+  for (var state in nowcast) {
+    var stateData = nowcast[state];
+    var s = {
+      initials: stateData[0],
+      name: stateData[30].split(" Statewide")[0],
+      ev: stateData[2],
+      dem: stateData[8],
+      rep: stateData[9],
+      moe: stateData[31],
+      districts: []
+    }
+    if (_.isNull(stateData[32])) {
+      data[s.initials] = s;
+    }
+    else {
+      var parentState = s.name.split(" ")[0];
+      _.find(data, function(p){ return p.name === parentState }).districts.push(s);
+    }
+  }
+  
+  return data;
+}
---
+++ montecarlo.js
@@ -0,0 +1,86 @@
+var simulate = function(data, runs) {
+  var CONST_runs = runs || 2000;
+  
+  var projected = {
+    evDem: 0,
+    evRep: 0,
+    wDem: 0,
+    wRep: 0,
+    tied: 0,
+    states: {}
+  };
+  
+  for (var state in data) {
+    projected.states[data[state].initials] = { wDem: 0, wRep: 0 };
+    
+    for (var district in data[state].districts) {
+      projected.states[data[state].districts[district].initials] = { wDem: 0, wRep: 0 };
+    }
+  }
+  
+  for (var iteration = 1; iteration <= CONST_runs; iteration++) {
+    var projection = { evDem:0, evRep: 0 }
+    
+    for (var state in data) {
+      var stateData = data[state];
+
+      var vsDem = (stateData.dem);
+      var vsRep = (stateData.rep);
+      var moe = stateData.moe + 2;
+
+      var pDem = jstat.pnorm((vsDem - vsRep) / moe);
+      var pRep = jstat.pnorm((vsRep - vsDem) / moe);
+
+      var roll = Math.random();
+      if (roll < pDem) {
+        projection.evDem += stateData.ev
+        projected.states[stateData.initials].wDem++;
+      }
+      else {
+        projection.evRep += stateData.ev
+        projected.states[stateData.initials].wRep++;
+      }
+      
+      for (var district in stateData.districts) {
+        var districtData = stateData.districts[district];
+        
+        var dvsDem = districtData.dem;
+        var dvsRep = districtData.rep;
+        var dmoe = districtData.moe + 2;
+
+        var dpDem = jstat.pnorm((dvsDem - dvsRep) / moe);
+        var dpRep = jstat.pnorm((dvsRep - dvsDem) / moe);
+
+        var droll = Math.random();
+        if (roll < dpDem) {
+          projection.evDem += districtData.ev
+          projected.states[districtData.initials].wDem++;
+        }
+        else {
+          projection.evRep += districtData.ev
+          projected.states[districtData.initials].wRep++;
+        }
+      }
+    }
+    
+    projected.evDem += projection.evDem;
+    projected.evRep += projection.evRep;
+    (projection.evDem > projection.evRep ? projected.wDem++ : 
+      projection.evDem !== projection.evRep ? projected.wRep++ : projected.tied++);
+      
+    if (iteration % 500 == 0) {
+      console.log((iteration/CONST_runs * 100) + "%")
+    }
+  }
+  
+  return { 
+    evDem: (projected.evDem / CONST_runs), 
+    evRep: (projected.evRep / CONST_runs), 
+    wDem: (projected.wDem / CONST_runs), 
+    wRep: (projected.wRep / CONST_runs), 
+    wTied: (projected.tied / CONST_runs),
+    states: _.each(projected.states, function(s){
+      return { wDem: (s.wDem/CONST_runs), wRep: (s.wRep/CONST_runs) };
+    })
+  };
+}
